c3da8fcd8b79397308cdbe881805e3fe230e4f14
rishabhkukreja30 <rishabhkukreja30@gmail.com>
2024-01-04 16:49:41 +0530

    added todo  end-points to view CSRF issue

---
+++ src/main/java/com/example/springsecurity/todo/TodoController.java
@@ -0,0 +1,35 @@
+package com.example.springsecurity.todo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TodoController {
+
+
+    Logger logger = LoggerFactory.getLogger(TodoController.class);
+
+    private static List<Todo> TO_DO_LIST = List.of(new Todo("user", "Hi, there!"),
+            new Todo("user2", "Hi There 2!"));
+
+    @GetMapping("/todos")
+    public List<Todo> retreiveAllTodos() {
+        return TO_DO_LIST;
+    }
+
+    @GetMapping("/users/{username}/todos")
+    public Todo retreiveTodoForSprcificUser(@PathVariable String username) {
+        return TO_DO_LIST.get(0);
+    }
+
+    @PostMapping("/users/{username}/todos")
+    public void createTodoForSprcificUser(@PathVariable String username, @RequestBody Todo todo) {
+        logger.info("Create {} for {}", todo, username);
+        TO_DO_LIST.add(todo);
+    }
+
+
+}
