b0e43d5bede99bd7e6aa4c95844ae72542647f19
rishabhkukreja30 <rishabhkukreja30@gmail.com>
2024-01-04 22:47:12 +0530

    added config to store user details in H2 database

--- src/main/java/com/example/springsecurity/resources/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/example/springsecurity/resources/BasicAuthSecurityConfiguration.java
@@ -2,16 +2,22 @@ package com.example.springsecurity.resources;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class BasicAuthSecurityConfiguration {
 
 //    adding configurations to store user details
     @Bean
-    public UserDetailsService userDetailsService() {
+    public UserDetailsService userDetailsService(DataSource dataSource) {
         var user = User.withUsername("rishabh")
                 .password("{noop}12345")
                 .roles("USER")
@@ -22,7 +28,21 @@ public class BasicAuthSecurityConfiguration {
                 .roles("ADMIN")
                 .build();
 
-        return  new InMemoryUserDetailsManager(user, admin);
+        var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(user);
+        jdbcUserDetailsManager.createUser(admin);
+
+        return jdbcUserDetailsManager;
+
+//        return  new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
     }
 
 }
